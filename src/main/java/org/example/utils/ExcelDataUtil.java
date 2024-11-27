f5cb39bf158b2ba624c7579a5a21da0d6ea0d986
FFFamily <49579222+FFFamily@users.noreply.github.com>
2024-11-27 18:24:37 +0800

    fix

--- src/main/java/org/example/utils/ExcelDataUtil.java
+++ src/main/java/org/example/utils/ExcelDataUtil.java
@@ -21,31 +21,38 @@ public class ExcelDataUtil {
         List<SourceFileData> sourceFileDataList = new ArrayList<>();
         EasyExcel.read(filePath, SourceFileData.class, new PageReadListener<SourceFileData>(dataList -> {
             dataList.forEach(i -> {
-                String matchField = getValue(i.getSEGMENT1_NAME())  + "." +
-                        getValue(i.getSEGMENT2_NAME()) + "." +
-                        getValue(i.getSEGMENT3_NAME()) + "." +
-                        getValue(i.getSEGMENT4_NAME()) + "." +
-                        getValue(i.getSEGMENT5_NAME()) + "." +
-                        getValue(i.getSEGMENT6_NAME()) + "." +
-                        getValue(i.getSEGMENT7_NAME()) + "." +
-                        getValue(i.getSEGMENT8_NAME()) + "." +
-                        getValue(i.getSEGMENT9_NAME()) + "." +
-                        getValue(i.getSEGMENT10_NAME());
+                if (i.getSEGMENT3_NAME().startsWith("应付账款")
+                || i.getSEGMENT3_NAME().startsWith("预付账款")
+                        || i.getSEGMENT3_NAME().startsWith("合同负债")
+                        || i.getSEGMENT3_NAME().startsWith("应收账款")
+                        || i.getSEGMENT3_NAME().startsWith("其他应付款")
+                        || i.getSEGMENT3_NAME().startsWith("其他应收款")){
+                    String matchField = getValue(i.getSEGMENT1_NAME())  + "." +
+                            getValue(i.getSEGMENT2_NAME()) + "." +
+                            getValue(i.getSEGMENT3_NAME()) + "." +
+                            getValue(i.getSEGMENT4_NAME()) + "." +
+                            getValue(i.getSEGMENT5_NAME()) + "." +
+                            getValue(i.getSEGMENT6_NAME()) + "." +
+                            getValue(i.getSEGMENT7_NAME()) + "." +
+                            getValue(i.getSEGMENT8_NAME()) + "." +
+                            getValue(i.getSEGMENT9_NAME()) + "." +
+                            getValue(i.getSEGMENT10_NAME());
 //                        getValue(i.getTransactionObjectCode()) + "." +
 //                        getValue(i.getTransactionObjectName());
-                String matchFieldCode = getValue(i.getSEGMENT1())  + "." +
-                        getValue(i.getSEGMENT2()) + "." +
-                        getValue(i.getSEGMENT3()) + "." +
-                        getValue(i.getSEGMENT4()) + "." +
-                        getValue(i.getSEGMENT5()) + "." +
-                        getValue(i.getSEGMENT6()) + "." +
-                        getValue(i.getSEGMENT7()) + "." +
-                        getValue(i.getSEGMENT8()) + "." +
-                        getValue(i.getSEGMENT9()) + "." +
-                        getValue(i.getSEGMENT10());
-                i.setMatch(matchFieldCode);
-                i.setMatchName(matchField);
-                sourceFileDataList.add(i);
+                    String matchFieldCode = getValue(i.getSEGMENT1())  + "." +
+                            getValue(i.getSEGMENT2()) + "." +
+                            getValue(i.getSEGMENT3()) + "." +
+                            getValue(i.getSEGMENT4()) + "." +
+                            getValue(i.getSEGMENT5()) + "." +
+                            getValue(i.getSEGMENT6()) + "." +
+                            getValue(i.getSEGMENT7()) + "." +
+                            getValue(i.getSEGMENT8()) + "." +
+                            getValue(i.getSEGMENT9()) + "." +
+                            getValue(i.getSEGMENT10());
+                    i.setMatch(matchFieldCode);
+                    i.setMatchName(matchField);
+                    sourceFileDataList.add(i);
+                }
             });
         })).sheet(sheetName).doRead();
         System.out.println("9月科目辅助余额表 读取完成");
